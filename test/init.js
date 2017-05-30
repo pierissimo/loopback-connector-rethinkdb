a706ed2293f2316fdf409c55998c5d71c18f5644
Piero Maltese <maltesepiero@gmail.com>
2017-05-30 11:36:56 +0100

    chore: some test cleanup

--- test/init.js
+++ test/init.js
@@ -3,25 +3,15 @@ module.exports = require('should');
 var DataSource = require('loopback-datasource-juggler').DataSource;
 
 var config = {
-        host: "localhost",
-        port: 28015,
-        db: "test"
-    };
-
-/*if (process.env.CI) {
-  config = {
-    host: 'localhost',
-    database: 'lb-ds-mongodb-test-' + (
-      process.env.TRAVIS_BUILD_NUMBER || process.env.BUILD_NUMBER || '1'
-    ),
-  };
-}*/
+  host: "localhost",
+  port: 28015,
+  db: "test"
+};
 
 process.env.TESTING = process.env.CI || process.env.NODE_ENV;
 
 global.getDataSource = global.getSchema = function (customConfig) {
   var db = new DataSource(require('../'), customConfig || config);
-  //var db = new DataSource(require('loopback-datasource-juggler/lib/connectors/memory'));
   db.log = function (a) {
     console.log(a);
   };
